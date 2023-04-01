dc8f9efaa33aa56c7407e8eb9e5f9f61686d471b
etcd <etcd>
2023-04-01 16:34:20 -0400

    Basic tokenizer

--- src/mygpt.py
+++ src/mygpt.py
@@ -1,13 +1,21 @@
 # curl https://raw.githubusercontent.com/karpathy/char-rnn/master/data/tinyshakespeare/input.txt > tinyshakespeare.txt
 
-with open("tinyshakespeare.txt", "r", encoding="utf-8") as f:
+from basic_tokenizer import make_encoder, make_decoder
+
+with open("src/tinyshakespeare.txt", "r", encoding="utf-8") as f:
     text = f.read()
 
 print("corpus length:", len(text))
 
 # print(text[:1000])
 
-chars = sorted(list(set(text)))
-vocab_size = len(chars)
-print("".join(chars))
-print(vocab_size)
+alphabet = sorted(list(set(text)))
+alphabet_size = len(alphabet)
+print("".join(alphabet))
+print(alphabet_size)
+
+encode = make_encoder(alphabet)
+decode = make_decoder(alphabet)
+
+print(encode("hello"))
+print(decode(encode("hello")))
