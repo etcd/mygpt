38adccbf0590f65da56bec055df91a750c14db96
etcd <etcd>
2023-04-01 17:25:49 -0400

    Get batch

--- src/mygpt.py
+++ src/mygpt.py
@@ -2,25 +2,38 @@
 from basic_tokenizer import make_encoder, make_decoder
 import torch
 
+
+def split_list(list, fraction):
+    n = int(len(list) * fraction)
+    return [list[:n], list[n:]]
+
+
+def get_batch(data, block_size, batch_size):
+    '''Gets a random batch of blocks from data.
+    '''
+    idx = torch.randint(len(data)-block_size, (batch_size,))
+    blocks = torch.stack([data[i:i+block_size] for i in idx])
+    targetss = torch.stack([data[i+1:i+block_size+1] for i in idx])
+    return blocks, targetss
+
+
 with open("sample_data/tinyshakespeare.txt", "r", encoding="utf-8") as f:
     text = f.read()
-
-print("corpus size", len(text))
-
 alphabet = sorted(list(set(text)))
-alphabet_size = len(alphabet)
-
-print("Alphabet", "".join(alphabet))
-print("Alphabet size", alphabet_size)
-
 encode = make_encoder(alphabet)
 decode = make_decoder(alphabet)
-
 data = torch.tensor(encode(text), dtype=torch.long)
 
-print(data.shape, data.dtype)
-print(data[:1000])
+[train_data, validate_data] = split_list(data, 0.9)
 
-n = int(len(data) * 0.9)
-train_data = data[:n]
-validate_data = data[n:]
+block_size = 8
+batch_size = 4
+
+blocks, targetss = get_batch(train_data, block_size, batch_size)
+print('blocks', blocks)
+print('targetss', targetss)
+
+for i in range(batch_size):
+    block = blocks[i]
+    targets = targetss[i]
+    print("in", block, "out", targets)
