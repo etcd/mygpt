4e834425771f6e73d8f1d9528c843485ff29de51
etcd <etcd>
2023-04-01 18:01:56 -0400

    make basic generator

--- src/mygpt.py
+++ src/mygpt.py
@@ -35,12 +35,35 @@ token_embedding_table = torch.nn.Embedding(
     len(alphabet), len(alphabet))  # batch, time, channels (vocab size)
 
 
-def forward(token_embedding_table, blocks, targetss):
+def forward(token_embedding_table, blocks, targetss=None):
     logits = token_embedding_table(blocks)
-    loss = torch.nn.functional.cross_entropy(
-        logits.view(-1, len(alphabet)), targetss.view(-1))
+    if targetss is None:
+        loss = None
+    else:
+        loss = torch.nn.functional.cross_entropy(
+            logits.view(-1, len(alphabet)), targetss.view(-1))
     return logits, loss
 
 
+def generate(idx, targetss, max_new_tokens):
+    # idx is (B, T) array of indices in current block
+    for _ in range(max_new_tokens):
+        # get predictions
+        logits, loss = forward(token_embedding_table, idx, targetss)
+        # get last token
+        logits = logits[:, -1, :]  # (B, C)
+        # apply softmax to get probabilities
+        probabilities = torch.nn.functional.softmax(logits, dim=1)  # (B, C)
+        # sample from probabilities
+        idx_next = torch.multinomial(probabilities, num_samples=1)  # (B, 1)
+        # append to idx
+        idx = torch.cat([idx, idx_next], dim=1)  # (B, T+1)
+    return idx
+
+
 logits, loss = forward(token_embedding_table, blocks, targetss)
-print(logits, loss)
+print(logits)
+
+# idx = torch.zeros((1, 1), dtype=torch.long)
+# print(decode(generate(idx, targetss=None,
+#       max_new_tokens=100)[0].tolist()))
