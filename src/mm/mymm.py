32d4514aaed11ecd92cda54307cb2e98233b0777
etcd <etcd>
2023-04-01 18:21:32 -0400

    basic bigrams and import torch

--- src/mm/mymm.py
+++ src/mm/mymm.py
@@ -1,2 +1,12 @@
-names = open('sample_data/names.txt', 'r').read().splitlines()
-print(len(names))
+import torch
+
+words = open('sample_data/names.txt', 'r').read().splitlines()
+
+b = {}
+for word in words:
+    chars = ['<S>'] + list(word) + ["<E>"]
+    for c1, c2 in zip(chars, chars[1:]):
+        bigram = (c1, c2)
+        b[bigram] = b.get(bigram, 0) + 1
+
+print(sorted(b.items(), key=lambda kv: kv[1]))
