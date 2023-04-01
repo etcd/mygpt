07304fa26749644d0d57f9797555622800364ebf
etcd <etcd>
2023-04-01 16:38:35 -0400

    Clean

--- src/mygpt.py
+++ src/mygpt.py
@@ -4,15 +4,13 @@ from basic_tokenizer import make_encoder, make_decoder
 with open("sample_data/tinyshakespeare.txt", "r", encoding="utf-8") as f:
     text = f.read()
 
-print("corpus length:", len(text))
+print("corpus size", len(text))
 
 alphabet = sorted(list(set(text)))
 alphabet_size = len(alphabet)
-print("".join(alphabet))
-print(alphabet_size)
+
+print("Alphabet", "".join(alphabet))
+print("Alphabet size", alphabet_size)
 
 encode = make_encoder(alphabet)
 decode = make_decoder(alphabet)
-
-print(encode("hello"))
-print(decode(encode("hello")))
