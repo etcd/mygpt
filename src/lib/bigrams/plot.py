91b6b320a2e8fa9a3ab8c90dba42f06acf3d2600
etcd <etcd>
2023-04-02 18:22:50 -0400

    start on NN code

--- src/lib/bigrams/plot.py
+++ src/lib/bigrams/plot.py
@@ -3,14 +3,14 @@ from matplotlib import pyplot as plt
 from torch import Tensor
 
 
-def plot_bigrams(bigrams: Tensor, decode: Callable[[list[int]], str]):
+def plot_bigrams(encoded_bigrams: Tensor, decode: Callable[[list[int]], str]):
     plt.figure(figsize=(15, 15))
-    plt.imshow(bigrams, cmap="Blues")  # type: ignore
+    plt.imshow(encoded_bigrams, cmap="Blues")  # type: ignore
     for x in range(27):
         for y in range(27):
             plt.text(x, y, decode([y, x]),  # type: ignore
                      ha='center', va='bottom', color="gray")
-            plt.text(x, y, bigrams[y, x].item(),  # type: ignore
+            plt.text(x, y, encoded_bigrams[y, x].item(),  # type: ignore
                      ha='center', va='top', color="gray")
     plt.axis('off')  # type: ignore
     plt.show()
--- src/mymm.py
+++ src/mymm.py
@@ -1,4 +1,6 @@
+import torch
 from lib.basic_tokenizer import make_tokenizers
+from lib.bigrams.get_bigrams import get_bigrams
 from lib.bigrams.get_log_likelihood import get_log_likelihood
 from lib.bigrams.count import count_bigrams
 from lib.math.get_perplexity import get_perplexity
@@ -27,3 +29,23 @@ for word in words:
 
 print(f"Average NLL: {-log_likelihood/num_bigrams:.4f}")
 print(f"Perplexity: {get_perplexity(log_likelihood, num_bigrams):.4f}")
+
+xs, ys = [], []
+for word in words[:1]:
+    encoded_word = encode('.' + word + '.')
+    encoded_bigrams = get_bigrams(encoded_word)
+    for c1, c2 in encoded_bigrams:
+        xs.append(c1)
+        ys.append(c2)
+
+xs_tensor = torch.tensor(xs)
+ys_tensor = torch.tensor(ys)
+
+xenc = torch.nn.functional.one_hot(xs_tensor, num_classes=27).float()
+print(xenc, xenc.shape)
+
+
+W = torch.randn(27, 27)
+logits = xenc @ W  # log counts
+prob = bigrams / bigrams.sum(1, keepdim=True)
+print(logits.exp())
