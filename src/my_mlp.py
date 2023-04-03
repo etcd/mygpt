f838c94351bba517ac17d2c5148a63f72007beb8
etcd <etcd>
2023-04-03 00:37:29 -0400

    clean

--- src/my_mlp.py
+++ src/my_mlp.py
@@ -1,19 +1,16 @@
 from lib.basic_tokenizer import make_tokenizers
-from lib.bigrams.get_bigrams import get_bigrams
 import torch
-from lib.nn.generate_word import generate_word
 
-from lib.nn.softmax import get_softmax
+
+block_size = 3
+embed_dims = 2
+hyper_dims = 100
 
 words = open('sample_data/names.txt', 'r').read().splitlines()
 alphabet = ['.'] + sorted(list(set(''.join(words))))
 alphabet_size = len(alphabet)
 (encode, decode) = make_tokenizers(alphabet)
 encoded_words = [encode(word + '.') for word in words]
-
-block_size = 3
-embedding_dims = 2
-hyper_dims = 100
 
 
 def make_samples(encoded_words: list[list[int]]):
@@ -33,46 +30,44 @@ xs_list, ys_list = make_samples(encoded_words)
 xs = torch.tensor(xs_list)
 ys = torch.tensor(ys_list)
 
-embedding_map = torch.randn(
-    (alphabet_size, embedding_dims), requires_grad=True)
+embed_weights = torch.randn((alphabet_size, embed_dims))
 
-hyper_weights = torch.randn(
-    (block_size*embedding_dims, hyper_dims), requires_grad=True)
-hyper_biases = torch.randn(hyper_dims, requires_grad=True)
+hyper_weights = torch.randn((block_size*embed_dims, hyper_dims))
+hyper_biases = torch.randn(hyper_dims)
 
-hyper_weights2 = torch.randn((hyper_dims, alphabet_size), requires_grad=True)
-hyper_biases2 = torch.randn(alphabet_size, requires_grad=True)
+out_weights = torch.randn((hyper_dims, alphabet_size))
+out_biases = torch.randn(alphabet_size)
 
-parameters = [embedding_map, hyper_weights,
-              hyper_biases, hyper_weights2, hyper_biases2]
+params = [embed_weights, hyper_weights, hyper_biases, out_weights, out_biases]
 
-parameter_count = sum(p.nelement() for p in parameters)
-print("Param count", parameter_count)
+for p in params:
+    p.requires_grad = True
+
+print("Param count", sum(p.nelement() for p in params))
 
 for _ in range(1000):
     # minibatch
     idxs = torch.randint(0, len(xs), (32,))
 
     # forward pass
-    emb = embedding_map[xs[idxs]]  # (token count, block_size, embedding_dims)
+    emb = embed_weights[xs[idxs]]  # (token count, block_size, embed_dims)
     h = torch.tanh(emb.view(-1, 6) @ hyper_weights + hyper_biases)
-    logits = h @ hyper_weights2 + hyper_biases2
+    logits = h @ out_weights + out_biases
     loss = torch.nn.functional.cross_entropy(logits, ys[idxs])
     # print(loss.item())
 
     # backward pass
-    for p in parameters:
+    for p in params:
         p.grad = None
     loss.backward()
 
     # update
-    for p in parameters:
+    for p in params:
         p.data -= 0.1 * p.grad  # type: ignore
 
 # total loss
-
-emb = embedding_map[xs]
+emb = embed_weights[xs]
 h = torch.tanh(emb.view(-1, 6) @ hyper_weights + hyper_biases)
-logits = h @ hyper_weights2 + hyper_biases2
+logits = h @ out_weights + out_biases
 loss = torch.nn.functional.cross_entropy(logits, ys)
 print(loss.item())
